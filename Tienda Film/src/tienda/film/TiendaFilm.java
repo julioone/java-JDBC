3c248726fda467581a3a1e345236e5fcf84cdb6d
julio.ospina <julioone@utp.edu.co>
2019-03-05 13:51:28 -0500

    Tienda Film usando JDBC y swing

---
+++ Tienda Film/src/tienda/film/TiendaFilm.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tienda.film;
+import java.sql.*;
+/**
+ *
+ * @author julio.ospina
+ */
+public class TiendaFilm {
+
+    /**
+     * @param args the command line arguments
+     */
+    static final String DATABASE_URL = "jdbc:mysql://localhost:3306/sakila";
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Principal myventana = new Principal();
+        myventana.setVisible(true);
+    }
+    
+}
